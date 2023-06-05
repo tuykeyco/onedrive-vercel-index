c5299c8fc9abd89175ffe660ec30018e34fe35fb
tuykeyco <135627680+tuykeyco@users.noreply.github.com>
2023-06-05 22:38:05 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   clientId: '40705372-60a1-48a4-af5d-0a190dece7e0',
+  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
   redirectUri: 'http://localhost',
   base: '/Public',
   authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
   driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
+  scope: 'user.read files.read.all offline_access',
+  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
 }
